03541ebde6a7d69051b151e76d6909f3f310de2f
Lucas Nouet <lguilenia@gmail.com>
2026-09-08 11:42:50 -0300

    fix: expose historical Elo endpoint

--- src/routes/ranking.routes.js
+++ src/routes/ranking.routes.js
@@ -1,7 +1,20 @@
 import { Router } from "express";
-import { getRanking } from "../controllers/ranking.controllers.js";
+
+import {
+  getRanking,
+  getHistoricalElo,
+} from "../controllers/ranking.controllers.js";
 
 const router = Router();
-router.get("/ranking", getRanking);
+
+router.get(
+  "/ranking",
+  getRanking,
+);
+
+router.get(
+  "/ranking/historical-elo",
+  getHistoricalElo,
+);
 
 export default router;
